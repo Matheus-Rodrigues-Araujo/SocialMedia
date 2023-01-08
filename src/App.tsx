3febf86733a6d9378ebae81c54d5810e26fcd251
Matheus-Rodrigues-Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2023-01-07 23:48:08 -0300

    first commit on master

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Header } from './components/header';
+import { About } from './pages/about';
+import { Contact } from './pages/contact';
+import { Home } from './pages/home';
+import { Login } from './pages/login';
+import { Register } from './pages/register';
 
+import './styles/App.css';
+import './styles/Header.css';
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About />}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/register' element={<Register />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,41 @@
+import {Link} from 'react-router-dom'
+export const Header = () =>{
+  const links = [
+    {
+      name: 'Home',
+      path: '/'
+    },
+    {
+      name: 'About',
+      path: '/about'
+    },
+    {
+      name: 'Contact',
+      path: '/contact'
+    },
+    {
+      name: 'Register',
+      path: '/register'
+    },
+    {
+      name: 'Login',
+      path: '/login'
+    }
+  ]
+
+  return (
+    <header className="header">
+      <nav className='navbar'>
+        <Link to='/' className='logo' >Logo</Link>
+        <ul className='links-list'>
+          {
+            links.map((link, key) => (
+            <li key={key} >
+              <Link to={link.path} >{link.name}</Link>
+            </li>))
+          }
+        </ul>
+      </nav>
+    </header>
+  )
+}
