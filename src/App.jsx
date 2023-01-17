79fcd57dbc6eb3bf28bab5090d82e0a489e55fe5
Matheus Rodrigues Araujo <matheus.rodgs.araujo13@gmail.com>
2023-01-16 23:15:59 -0300

    axios installed

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,6 @@ function App() {
     <div className="App">
       <Router>
         <Header/>
-
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/register' element={<Register />}/>
