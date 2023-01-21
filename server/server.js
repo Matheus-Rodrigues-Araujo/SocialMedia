1d299f5cfd57dbdc1bf407fd02779fa9d9a9e293
Matheus Rodrigues Araujo <matheus.rodgs.araujo13@gmail.com>
2023-01-21 17:41:20 -0300

    .gitignore updated

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-require("dotenv").config({path: "./config.env"})
+require("dotenv").config({path: "./env/config.env"})
 const port = process.env.port || 4000
 const cors = require('cors')
 app.use(cors())
