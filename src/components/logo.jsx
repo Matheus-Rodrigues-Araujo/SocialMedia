d4adc20d6aa51a06aa5acaf992e73c41a9ec7ab0
Matheus Rodrigues Araujo <matheus.rodgs.araujo13@gmail.com>
2023-01-27 21:20:46 -0300

    NEW: logo.jsx component, forms folder and images folder

---
+++ src/components/logo.jsx
@@ -0,0 +1,11 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faComment } from "@fortawesome/free-solid-svg-icons"
+import { Link } from 'react-router-dom'
+export const Logo = () => {
+    return (
+        <div className='logo-conteiner' >
+            <FontAwesomeIcon icon={faComment} />
+            <Link to='/' className='logo'>Talk Now!</Link>
+        </div>
+    )
+}
---
+++ src/forms/signin.jsx
@@ -0,0 +1,63 @@
+import { useNavigate, Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import * as yup  from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useSelector, useDispatch} from 'react-redux';
+import Axios from 'axios';
+import { authenticateUser } from '../features/user/userSlice';
+import { Logo } from '../components/logo';
+
+const schema = yup.object({
+  email: yup.string().required('Email is required!'),
+  password: yup.string().required('Password is required!')
+}).required()
+
+export const SignIn = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const { register,handleSubmit, formState: {errors} } = useForm({resolver: yupResolver(schema)})
+
+  const onSubmit = async (formValues) =>{
+    const {email, password} = formValues
+    const url = "http://localhost:4000/api/login"
+    const data = {email: email, password: password}
+    const config = {'content-type': 'application/json'}
+    
+    await Axios.post(url, data, config)
+    .then(res=>{
+      dispatch(
+        authenticateUser(res.data)
+      )
+    }).then(()=>navigate('/welcome'))
+    .catch(error =>{
+      console.log(error)
+    }) 
+  }
+
+  return(
+    <form className="form"
+      onSubmit={handleSubmit(onSubmit)}>
+      <Logo />
+      <h1>Welcome</h1>
+      
+      <div className="fields" >
+        <label>
+          Email
+          <input type='text' placeholder="...Email" {...register('email')}/>
+          <p className='error-msg' >{errors.email?.message}</p>
+        </label>
+
+        <label>
+          Password
+          <input type='password' placeholder="...Password" {...register('password')} />
+          <p className='error-msg' >{errors.password?.message}</p>
+        </label>
+
+        <input type='submit' className="btn-styled" value='Login' />
+        <Link to="/register" className='forgot-password' >Forgot password?</Link>
+        <p className='create-account-link' >Don't have an account? <Link to="/register"> Create account</Link></p>
+      </div>
+    </form>
+  )
+}
