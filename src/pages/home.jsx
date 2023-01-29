6d53173e759d1b3d1a7a54db967aa8ed371d7c56
Matheus Rodrigues Araujo <matheus.rodgs.araujo13@gmail.com>
2023-01-29 12:56:14 -0300

    SignIn form with new design

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,28 +1,7 @@
-import { useSelector } from "react-redux"
-import Axios from "axios"
-import { useEffect, useState } from "react"
 import { SignIn } from "../forms/signin"
 import img1 from '../images/img1.jpg'
-import img2 from '../images/img2.jpg'
-import img3 from '../images/img3.jpg'
-import img4 from '../images/img4.jpg'
 
 export const Home = () =>{
-  // const isLogged = useSelector((state)=> state.user.isLogged)
-  const [usersList, setUsersList] = useState([])
-  // const images = [img2, img3,img4]
-
-  // const serverConnection = () =>{
-  //   const url = 'http://localhost:4000/api';
-  //   const config= {'content-type': 'application/json'}
-  //   Axios.get(url, config).then(res => {
-  //     setUsersList(res.data)
-  //   })
-  // }
-
-  // useEffect(()=>{
-  //   serverConnection()
-  // }, [])
 
   return(
     <div className="home">
