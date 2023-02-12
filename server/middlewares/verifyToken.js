0e83b0995cd3968b5ea7696b91b5247570756cca
Matheus Rodrigues Araujo <matheus.rodgs.araujo13@gmail.com>
2023-02-12 18:41:09 -0300

    client updated

--- server/middlewares/verifyToken.js
+++ server/middlewares/verifyToken.js
@@ -1,1 +1,20 @@
 const jwt = require('jsonwebtoken')
+
+const verifyToken = (req, res, next) =>{
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if(!token){
+        return res.status(403).json({msg: 'Invalid Token!'})
+    }
+    
+    jwt.verify(token, process.env.JWT_SECRET, (err, data)=>{
+        if(err) return res.status(403).json({msg: 'Wrong or expired token'})
+        req.user = data
+        next()
+    })
+
+
+}
+
+module.exports = verifyToken
--- server/server.js
+++ server/server.js
@@ -1,20 +1,28 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-require("dotenv").config({path: ".env"})
 const cors = require('cors')
 const port = process.env.port || 4000
+const dotenv = require('dotenv')
+dotenv.config({path: '.env'})
 
+
+const authController = require('./controllers/auth')
+const commentController = require('./controllers/comment')
+const postController = require('./controllers/post')
+const userController = require('./controllers/user')
+
+mongoose.connect(process.env.ATLAS_URI, ()=> console.log('>>> DB is connected successfully!'))
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.use('/auth', authController)
+app.use('/comment', commentController)
+app.use('/post', postController)
+app.use('/user', userController)
 
-
-
-app.use(require("./routes/apiRoutes"))
-
-const dbo = require("./db/conn")
 
 app.listen(port, ()=>{
     dbo.connectToServer((err)=>{
