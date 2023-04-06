d221acd025e286316b82903145e65d53436256d9
Matheus Rodrigues Araujo <matheus.rodgs.araujo13@gmail.com>
2023-04-06 17:45:09 -0300

    CSS has been replaced witg SCSS

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,17 +4,17 @@ import App from './App';
 import { Provider } from 'react-redux';
 import {store} from './config/store'
 
-import "./styles/Main.css"
-import "./styles/App.css"
-import './styles/Home.css'
-import './styles/Welcome.css'
-import './styles/Forms/SignIn.css'
-import "./styles/Forms/SignUp.css"
-import './styles/Sidebar/Sidebar.css'
-import './styles/Logo.css'
-import './styles/Post/Post.css'
-import './styles/Profile/Profile.css'
-import './styles/NotFound.css'
+import "./styles/main.scss"
+import "./styles/app.scss"
+import './styles/home.scss'
+import './styles/welcome.scss'
+import './styles/Forms/signin.scss'
+import "./styles/Forms/signup.scss"
+import './styles/Sidebar/sidebar.scss'
+import './styles/logo.scss'
+import './styles/Post/post.scss'
+import './styles/Profile/profile.scss'
+import './styles/notfound.scss'
 
 
 const root = ReactDOM.createRoot(
